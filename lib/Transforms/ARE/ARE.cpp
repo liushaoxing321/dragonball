9cbacc73b688794ab29ec8c93a7646fd6a5d2d89
Leslie Zhai <lesliezhai@llvm.org.cn>
2017-09-01 14:42:42 +0800

    [ARE] rebase for LLVM v6.x (svn-r312303)

--- lib/Transforms/ARE/ARE.cpp
+++ lib/Transforms/ARE/ARE.cpp
@@ -22,7 +22,9 @@
 
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Analysis/CallGraphSCCPass.h"
+#include "llvm/Analysis/CallGraph.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 
